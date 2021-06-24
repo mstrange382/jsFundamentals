4194d067e293eb4a2aa388f371a7aa706e6ff2d4
Mitch Strange <mstrange382@gmail.com>
2021-06-23 22:16:32 -0500

    NYT

---
+++ HomeworkChallenges/whiteboard/test.js
@@ -0,0 +1,49 @@
+function favoriteResturants(rest1,rest2){
+    console.log(`My favorite places to eat are ${favoriteResturants}`);
+}
+
+favoriteResturants();
+
+
+for (let i=0; i <= 100; i++){
+    
+    if (i % 3 === 0){
+    console.log('${i} is divisible by 3')
+
+    }else if(i % 5 === 0){
+    console.log('${i} is divisible by 5')
+    } 
+    console.log('this number is divisible by 5');
+
+
+    console.log('this number is not divisible');
+}
+
+
+
+let bmvClient = {
+
+name:'Mitch Strange',
+address:'123 fake street',
+plate: {
+    vehicle: [{
+        year:2012
+        make:chevy
+        model:prizm
+        validplate: yes
+    },{
+        year:2012
+        make:chevy
+        model:prizm
+        validplate: yes
+    }]
+    }
+}
+
+
+
+function areaOfTrap(b1,b2,h){
+    console.log(.5 * (b1+b2)*h);
+}
+
+areaOfTrap(1,2,3);
--- javaScriptMay2021/4-Apps/01-New-York-Times/nyt.js
+++ javaScriptMay2021/4-Apps/01-New-York-Times/nyt.js
@@ -1,12 +1,12 @@
 const baseURL = 'https://api.nytimes.com/svc/search/v2/articlesearch.json';
-const key = 'enter key from email here';
+const key = 'bVcb8JVIUC5a83QjlJXUfrAQLWlPULWN';
 let url;
 
 //SEARCH FORM
 const searchTerm = document.querySelector('.search');
 const startDate = document.querySelector('.start-date');
 const endDate = document.querySelector('.end-date');
-const searchForm = document.querySelector('.form');
+const searchForm = document.querySelector('form');
 const submitBtn = document.querySelector('.submit');
 
 //RESULTS NAVIGATION
@@ -21,5 +21,100 @@ nav.style.display = 'none';
 
 let pageNumber = 0;
 let displayNav = false;
+
+
+searchForm.addEventListener('submit',fetchResults);
+nextBtn.addEventListener('click',nextPage);
+previousBtn.addEventListener('click',previousPage);
+
+function fetchResults(e){
+    console.log (e);
+    url = 'https://api.nytimes.com/svc/search/v2/articlesearch.jsonbVcb8JVIUC5a83QjlJXUfrAQLWlPULWNkey&page=pageNumber&q=searchTerm.value';
+    console.log(url);
+}
+function nextPage(){
+    console.log("Next button clicked.");
+}
+
+function previousPage(){
+    console.log("Next button clicked.");
+}
+
+
+function fetchResults(e){
+    e.preventDefault();
+    url = 'https://api.nytimes.com/svc/search/v2/articlesearch.jsonbVcb8JVIUC5a83QjlJXUfrAQLWlPULWNkey&page=pageNumber&q=searchTerm.value';
+    if(startDate.value !== ''){
+        console.log(startDate.value)
+        url += '&begin_date=' + startDate.value;
+    };                                         //form validation (optional?)
+    if(endDate.value !== ''){
+        url += '&end_date=' + endDate.value;
+    };
+    fetch(url)
+    .then (function(result){
+        return result.json();
+    }).then(function(json){
+        displayResults(json);
+    });
+//We make the  request.
+// We pass in the NYT .
+// We create a promise  that returns a response object called .
+// The promise asynchronously returns a function that converts the result into usable  format -  is that function call.
+// We create a second promise that has a function that takes in the  object.
+// We log the  object for now.
+
+
+
+}
+function displayResults(json) {
+    while (section.firstChild){
+        section.removeChild(sections.firstChild);
+    }
+    let articles = json.response.docs;
+
+    if (articles.length >=10){
+        nav.style.display = 'block';
+    } else{
+        nav.style.display = 'none';
+    }
+    
+    if (articles.length === 0){
+        console.log("No Results");
+    }else{
+        for(let i = 0; i<= articles.length; i++){
+            let article= document.createElement('article');
+            let heading = document.createElement('h2');
+            let link = document.createElement('a');
+            let para = document.createElement('p');
+            let clearfix = document.createElement('div');
+                                                            
+            let current = articles[i];
+            console.log("Current:", current);
+
+            link.href = current.web_url;
+            link.textContent = current.headline.main;
+
+            para.textContent = 'keywords:';
+            for (let j=0; j < current.keywords.length; j++){
+                let span = document.createElement('span');
+                span.textContent += current.keywords[j].value + '';
+                para.appendChild(span);
+            }
+            clearfix.setAttribute('class','clearfix');
+            
+
+            article.appendChild(heading);
+            heading.appendChild(link);
+            article.appendChild(para);
+            article.appendChild(clearfix);
+            section.appendChild(article);
+        }
+            }
+        };
+
+
+
+
 
 
